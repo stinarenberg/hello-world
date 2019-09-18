2cb641ac6b28514dfe47e5016944d97b403f3f01
stren0 <54666760+stren0@users.noreply.github.com>
2019-09-18 21:47:55 +0200

    final

--- script.js
+++ script.js
@@ -40,23 +40,15 @@ function toggleBtn2() {
 
 }
 
-
-
 function toggleBoxes() {
     const circleRightEl = document.getElementById('changingcircle--right');
     const circleWrongEl = document.getElementById('changingcircle--wrong');
 
     const circleRightIsExpanded = circleRightEl.classList.contains("changingcircle--expanded")
 
-    //Om vi just nu visar den "korrekta" cirkeln
     if (circleRightIsExpanded) {
         circleRightEl.classList.remove("changingcircle--expanded");
         circleWrongEl.classList.add("changingcircle--expanded");
-        // Tänk på att bevara övriga klasser
-        //Dölja den korrekta cirkeln
-        //Ta bort klassen changingcircle--expanded på rightEl
-        //Visa den inkorrekta
-        //Lägg till klassen changingcircle--expanded på wrongEl
     } else {
         circleRightEl.classList.add("changingcircle--expanded");
         circleWrongEl.classList.remove("changingcircle--expanded");
