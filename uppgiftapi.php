2b3dacdff09d4639018e23cf221d484075d3eb62
stren0 <54666760+stren0@users.noreply.github.com>
2019-10-10 11:46:12 +0200

    updat

--- uppgiftapi.php
+++ uppgiftapi.php
@@ -1,28 +1,58 @@
 <?php
-
 header("Content-Type: application/json; charset=UTF-8");
 
-
 $limit = 10;
+
 if(isset($_GET['limit'])){
     $limit = $_GET['limit']; 
 }
 
+$ros=[
+    "Produkt"=>"Ros", 
+    "Färg"=>"Röd", 
+    "Pris"=>"22kr", 
+    "Bild"=>"img", 
+    "Beskrivning"=>"Beskrivning"];
 
+$maskros=[
+    "Produkt"=>"maskros", 
+    "Färg"=>"gul", 
+    "Pris"=>"22kr", 
+    "Bild"=>"img", 
+    "Beskrivning"=>"Beskrivning"];
 
-$ros=["Produkt"=>"Ros", "Färg"=>"Röd", "Pris"=>"22kr", "Bild"=>"img", "Beskrivning"=>"Beskrivning"];
-$maskros=["maskros", "gul", "22kr", "img", "Beskrivning"];
-$orkidee=["orkidee", "vit", "22kr", "img", "Beskrivning"];
-$lilja=["Lilja", "Rosa", "22kr", "img", "Beskrivning"];
-$blaklocka=["Produkt"=>"Blåklocka", "Färg"=>"Blå", "Pris"=>"22kr", "Bild"=>"img", "Beskrivning"=>"Beskrivning"];
+$orkidee=[
+    "Produkt"=>"orkidee", 
+    "Färg"=>"vit", 
+    "Pris"=>"22kr", 
+    "Bild"=>"img", 
+    "Beskrivning"=>"Beskrivning"];
 
+$lilja=[
+    "Produkt"=>"Lilja", 
+    "Färg"=>"Rosa", 
+    "Pris"=>"22kr", 
+    "Bild"=>"img", 
+    "Beskrivning"=>"Beskrivning"];
 
-$blommor = array("ros"=>$ros, "maskros" => $maskros, "orkidee"=>$orkidee, "lilja"=>$lilja, "blåklocka"=>$blaklocka);
+$blaklocka=[
+    "Produkt"=>"Blåklocka",
+    "Färg"=>"Blå", 
+    "Pris"=>"22kr", 
+    "Bild"=>"img", 
+    "Beskrivning"=>"Beskrivning"];
+$blommor = array(
+    "ros"=> $ros, 
+    "maskros" => $maskros, 
+    "orkidee"=>$orkidee, 
+    "lilja"=>$lilja, 
+    "blåklocka"=>$blaklocka);
+
 //print_r($blommor);
+
 
 $json = json_encode($blommor, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT);
 
 echo $json;
-
 
 ?>
